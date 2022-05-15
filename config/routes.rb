7a8435af6cf7484ee0c93c9aabf03b280e417634
zahidalik <zahidkhan545@icloud.com>
2022-05-15 22:35:51 +0300

    set root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'basic-qr-code-reader', to: 'basic_qr_codes#index'
+  root 'basic_qr_codes#index'
 end
